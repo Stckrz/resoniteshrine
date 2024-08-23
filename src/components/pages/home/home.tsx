d2fe6910734440b8e1dc8b7ac75027641dd30eb5
Stckrz <rin449@gmail.com>
2024-08-23 12:46:56 -0500

    hi

--- src/components/pages/home/home.tsx
+++ src/components/pages/home/home.tsx
@@ -4,7 +4,6 @@ const Home: React.FC = () => {
 	return (
 		<div className="p-2">
 			<div className="w-1/2">
-			<img src="https://resoniteshrinebucket.s3.us-east-2.amazonaws.com/20240629143552_1.jpg"/>
 			</div>
 			homepage!
 		</div>
